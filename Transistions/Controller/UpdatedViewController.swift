b1c733fec621b2f986b8d4f35d3036e0498f71a3
JohnathanFlint <nicholas.white142@gmail.com>
2016-10-03 09:46:28 -0600

    Tried to fix the sideway top image.

--- Transistions/Controller/UpdatedViewController.swift
+++ Transistions/Controller/UpdatedViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class UpdatedViewController : UIViewController
 {
+    @IBOutlet weak var topImage: UIImageView!
+    @IBOutlet weak var bottomImage: UIImageView!
+    
+    
+    
     override func viewDidLoad()
     {
         //Load the parent version of thview.
         super.viewDidLoad()
         //Load this pages components.
+        bottomImage.image = UIImage(named: "IMG_5747")
+        //let tempImage = UIImage(named:"IMG_5745")
+        //let cgImage = tempImage?.CGImageForProposedRect(nil, context: nil, hints: nil)
     }
 }
