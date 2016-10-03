d0c12e8f3236a91d89ecc216d51fc9ec185618fd
JohnathanFlint <nicholas.white142@gmail.com>
2016-10-03 08:30:06 -0600

    Corrected the error in the button.

--- Transistions/ViewController.swift
+++ Transistions/ViewController.swift
@@ -23,7 +23,7 @@ UIViewController {
 
     @IBAction func SwitchScreenToUpdated(sender: UIButton)
     {
-        performSegueWithIdentifier("toUpdatedFromMain", sender:sender)
+        performSegueWithIdentifier("toUpdated", sender:sender)
     }
     
    
