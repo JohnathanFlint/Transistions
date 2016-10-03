fe15010ddf84f98e10fe0253cce883c6fe010d66
JohnathanFlint <nicholas.white142@gmail.com>
2016-10-03 08:15:24 -0600

    Showed the link in code to change screens.

--- Transistions/ViewController.swift
+++ Transistions/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController :
+UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func SwitchScreenToUpdated(sender: UIButton)
+    {
+        performSegueWithIdentifier
+        ("toUpdatedFromMain", sender:sender)
+    }
+    
+   
 
 }
 
